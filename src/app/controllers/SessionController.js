ac992b9e6f04d4083d7c0490d180959fc5f88d22
mjlbnu <mjlbnu@gmail.com>
2019-11-05 22:11:34 -0300

    Validação de dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,22 @@
+import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    // schema validation
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Validation fails.' });
+    }
+
     const { email, password } = req.body;
 
     // verifica se o email está cadastrado
